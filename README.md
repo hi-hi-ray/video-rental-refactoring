80e20901155ededfa53dafe12b45b15811bbbc0c
Raysa Dutra <rai.dietra4@yahoo.com>
2018-09-16 21:12:58 -0300

    refactor points

--- README.md
+++ README.md
@@ -72,7 +72,7 @@ No livro é comentado que devemos colocar o *frequentRenterPoints* e a forma que
 
 ##### Na nossa quinta refatoração, começamos o processo de extração do *frequentRenterPoints* e inserimento na classe *Rental*. Após essa mudança ser feita é perceptível que na lógica é necessário adicionar 2 caso ele responda a condição caso (*(getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1*).
 
-###### [Commit e Arquivo Relacionado]()
+###### [Commit e Arquivo Relacionado](https://github.com/hi-hi-ray/video-rental-refactoring/commit/c8d9380e1eb35f2b45533209ca1c9de24c695d9a#diff-04c6e90faac2675aa89e2176d2eec7d8)
 
 
 #### Replace a Temp with a Query
--- video-rental-refactoring/src/com/refactoring/Customer.java
+++ video-rental-refactoring/src/com/refactoring/Customer.java
@@ -21,13 +21,11 @@ public class Customer {
     }
 
     public String statement() {
-        int frequentRenterPoints = 0;
         Enumeration rentals = _rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
 
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
-            frequentRenterPoints += each.getFrequentRenterPoints();
 
             // show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t"
@@ -36,9 +34,8 @@ public class Customer {
 
         // add footer lines
         result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints)
+        result += "You earned " + String.valueOf(getTotalFrequentRenterPoints())
                 + " frequent renter points";
-
         return result;
     }
 
@@ -48,6 +45,16 @@ public class Customer {
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
             result += each.getCharge();
+        }
+        return result;
+    }
+    
+    private int getTotalFrequentRenterPoints() {
+        int result = 0;
+        Enumeration rentals = _rentals.elements();
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getFrequentRenterPoints();
         }
         return result;
     }
